3f372dae5d834494bd3f9e3f85ea96234f10677c
Stackbit Code Editor <projects@stackbit.com>
2021-04-08 12:18:21 +0000

    product.js: changed

--- src/layouts/product.js
+++ src/layouts/product.js
@@ -48,7 +48,7 @@ export default class Product extends React.Component {
                             })())}
                             {_.get(this.props, "page.frontmatter.on_sale", null) && {
                                 <div style=({marginBottom: 20})>
-                                    <span></span>
+                                    <span style=({})></span>
                                 </div>
                             }}
                             <div className="product__price">
